bb0ec30c6a946725e269d50b6636fb61e75ccf51
vishalkumarhere <vishalpalnesto@gmail.com>
2025-02-02 20:23:09 -0500

    Initial commit

--- src/train_v2.py
+++ src/train_v2.py
@@ -7,26 +7,26 @@ import joblib
 data = pd.read_csv('../data/sampregdata.csv')
 data.drop('Unnamed: 0', axis=1, inplace=True)
 print(data.corr())
-# x4 has highest correlation (-0.524)
+# x4 has highest correlation (-0.524) followed by x2 (-0.471)
 
-# 1b)
-x = data[['x4']]
-y = data['y']
+## 1c)
+x = data[['x2', 'x4']]
+y = data[['y']]
 
 # Train and save model
-x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.3, random_state=5)
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=42)
 
 model = LinearRegression().fit(x_train, y_train)
 y_pred = model.predict(x_test)
 
 print(f"MSE_1 = {mean_squared_error(y_test, y_pred):.2f}")
 print(f"R²_1 = {r2_score(y_test, y_pred):.2f}")
-# MSE_1 = 85.79
-# R²_1 = 0.26
+# MSE_1 = 53.62
+# R²_1 = 0.49
 
 # Save the model
-joblib.dump(model, '../models/model_v1.pkl')
-x_train.to_csv("../data/x_train1.csv", index=False)
-x_test.to_csv("../data/x_test1.csv", index=False)
+joblib.dump(model, '../models/model_v2.pkl')
+x_train.to_csv("../data/x_train2.csv", index=False)
+x_test.to_csv("../data/x_test2.csv", index=False)
 y_train.to_csv("../data/y_train.csv", index=False)
 y_test.to_csv("../data/y_test.csv", index=False)
